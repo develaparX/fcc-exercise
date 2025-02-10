7689e1b5b681c6eea5634da39d5039c0e3dc66e6
develaparX <srarfian@gmail.com>
2025-02-10 13:35:33 +0700

    add react quill to markdown-previewer

--- frontend-development-libraries/markdown-previewer/src/App.jsx
+++ frontend-development-libraries/markdown-previewer/src/App.jsx
@@ -2,32 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState("")
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1 className="mb-5 text-s ">Markdown Previewer FreeCodeCamp React</h1>
+     <div className='flex flex-row gap-3'>
+      <div className="bg-amber-300 w-2xl h-[70vh] flex items-center justify-center rounded-xl ">
+        {/* <textarea value={markdown} onChange={(e) =>setMarkdown(e.target.value)}  className='bg-white w-full h-full text-black p-8 rounded-xl '/> */}
+        <ReactQuill className='bg-white w-full h-full text-black rounded-xl overflow-auto' theme="snow" value={markdown} onChange={setMarkdown} />
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div  className='bg-blue-500 w-2xl h-[70vh] flex items-center justify-center rounded-xl ' dangerouslySetInnerHTML={{__html:markdown}}>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     </div>
     </>
   )
 }
